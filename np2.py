7afce136197c8f19843253df88257c096534fc42
code4jet <dipak01911@gmail.com>
2026-05-31 14:57:26 +0530

    Completed 10 ch.

--- np2.py
+++ np2.py
@@ -1,8 +1,15 @@
 import numpy as np
-
-p=np.array([])
-print(p)
-
-print(type(p))
-p+=2
-print(p)
+#  How to make transpose matrics 
+a=np.array([[4,5,5,6,3,5]])
+b=np.array([
+    [2,8,9],
+    [1,7,9],
+    [6,7,4],
+    [8,2,6]
+])
+print(a.shape)
+print(b.shape)
+print(a.T)
+print(b.T)
+print((a.T).shape)
+print((b.T).shape)
--- test.py
+++ test.py
@@ -11,3 +11,24 @@ print(sys.getsizeof(m[0]))
 
 y=np.array([4.9,5.6,6.4,7.1],dtype=np.int32)
 print(y)
+
+scores = np.array([80, 90, 70])
+weights = np.array([1, 2, 1])   # mid-term counts double
+
+x=np.average(scores)                       # → 80.0  (unweighted)
+y=np.average(scores, weights=weights)      # → 82.5  (weighted)
+z=np.average(scores, weights=weights, returned=False)  # → (82.5, 4.0)
+print(x,y,z)
+
+# NAN  and INF(infinity )
+import numpy as np
+
+x = np.array([1, 2, np.nan, 4])
+print(np.mean(x))   # result is NaN 
+
+print(np.nanmean(x)) # Ignore NaN
+y = np.array([1, 0, -1])
+  # [1.0, inf, -1.0]
+print(1 / y) 
+print(type(np.inf))
+
